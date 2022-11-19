19102d60419375fd45e84e62abb98597a5243e10
Ampsss123 <117526504+Ampsss123@users.noreply.github.com>
2022-11-19 08:33:24 +0530

    Add files via upload

---
+++ src/Books.js
@@ -0,0 +1,11 @@
+import React from "react";
+class My_Books extends React.Component{
+  render(){
+    return(
+      <div>
+        <h2>My books page</h2>
+      </div>
+    )
+  }
+}
+export default My_Books;
---
+++ src/Browse.js
@@ -0,0 +1,11 @@
+import React from "react";
+class Browse extends React.Component{
+  render(){
+    return(
+      <div>
+        <h2>The search page</h2>
+      </div>
+    )
+  }
+}
+export default Browse;
