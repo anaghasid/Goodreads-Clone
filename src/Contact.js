2aa487e0b66374b09227ebce520d5fe0446361d3
Ampsss123 <117526504+Ampsss123@users.noreply.github.com>
2022-11-25 18:34:17 +0530

    Update Contact.js

--- src/Contact.js
+++ src/Contact.js
@@ -1,8 +1,50 @@
-import React from "react";
-const Contact=()=>{
-	return(
-		<h1> This is contact us page</h1>
+import React, {useState} from "react"
+import "./Contact.css"
+import axios from "axios"
+import { useNavigate } from "react-router-dom"
 
-	)
+
+const Contact = ({ setContactUser}) => {
+
+    const history = useNavigate()
+// changed from useHistory
+    const [ user, setUser] = useState({
+		name:"",
+        email:"",
+        comments:""
+    })
+	const handleChange = e => {
+        const { name, value } = e.target
+        setUser({
+            ...user,
+            [name]: value
+        })
+    }
+	const contact = () => {
+        axios.post("http://localhost:3000/contact", user)
+        .then(res => {
+            alert(res.data.message)
+            setContactUser(res.data.user)
+
+        })
+    }
+
+
+
+    return (
+        <div className="contact">
+            <h1>Contact Us</h1>
+			
+			<input type="text" name="name" value={user.name} onChange={handleChange} placeholder="Enter your Name"></input>
+            <input type="text" name="email" value={user.email} onChange={handleChange} placeholder="Enter your Email"></input>
+			<form action="#">
+        <textarea rows="10" cols="50" name="comments" placeholder="Enter the subject">
+        </textarea>
+    </form>
+			<div className="button" onClick={contact}>Submit</div>
+        </div>
+		
+    )
 }
-export default Contact;
+
+export default Contact
