ef4c380dec732fef36823c782ee0f7810fea6b2d
anaghasid <106751197+anaghasid@users.noreply.github.com>
2022-11-27 15:31:00 +0530

    Update Stars.js

--- src/Stars.js
+++ src/Stars.js
@@ -1,82 +1,96 @@
-import React from "react";
+import React, {useState} from "react"
+import axios from "axios"
+import { useNavigate } from "react-router-dom"
 
 class Stars extends React.Component
 {
+  constructor(props){
+    super(props);
+    this.state = {bookname:this.props.bookname, stars:0, 
+    first:"https://s.gr-assets.com/images/layout/gr_red_star_inactive.png",
+  second:"https://s.gr-assets.com/images/layout/gr_red_star_inactive.png",
+third:"https://s.gr-assets.com/images/layout/gr_red_star_inactive.png",
+fourth: "https://s.gr-assets.com/images/layout/gr_red_star_inactive.png",
+fifth:"https://s.gr-assets.com/images/layout/gr_red_star_inactive.png"}
+
+  }
+
+  sendToDB()
+  {
+    // const uname = this.props.name
+    const stars = String(this.state.stars)
+    console.log(uname)
+    const senddata = {uname,stars}
+    console.log(senddata)
+    // CHANGE QWERTY HERE
+    axios.post("http://localhost:9002/home/qwerty", senddata)
+        .then(res => {
+            alert(res.data.message)
+        })
+  }
+  
   one()
   {
-    var first = document.getElementById('i1');
-    var second = document.getElementById('i2');
-    var third = document.getElementById('i3');
-    var fourth = document.getElementById('i4');
-    var fifth = document.getElementById('i5');
-    first.src = "https://s.gr-assets.com/images/layout/gr_red_star_active.png"
-    second.src = "https://s.gr-assets.com/images/layout/gr_red_star_inactive.png"
-    third.src = "https://s.gr-assets.com/images/layout/gr_red_star_inactive.png"
-    fourth.src = "https://s.gr-assets.com/images/layout/gr_red_star_inactive.png"
-    fifth.src = "https://s.gr-assets.com/images/layout/gr_red_star_inactive.png" 
+    this.setState({first: "https://s.gr-assets.com/images/layout/gr_red_star_active.png"})
+    this.setState({second: "https://s.gr-assets.com/images/layout/gr_red_star_inactive.png"})
+    this.setState({third: "https://s.gr-assets.com/images/layout/gr_red_star_inactive.png"})
+    this.setState({fourth: "https://s.gr-assets.com/images/layout/gr_red_star_inactive.png"})
+    this.setState({fifth: "https://s.gr-assets.com/images/layout/gr_red_star_inactive.png"})
+    this.setState({stars:1})
+    this.sendToDB()
   }
   two()
-  {
-    var first = document.getElementById('i1');
-    var second = document.getElementById('i2');
-    var third = document.getElementById('i3');
-    var fourth = document.getElementById('i4');
-    var fifth = document.getElementById('i5');
-    first.src = "https://s.gr-assets.com/images/layout/gr_red_star_active.png"
-    second.src = "https://s.gr-assets.com/images/layout/gr_red_star_active.png"
-    third.src = "https://s.gr-assets.com/images/layout/gr_red_star_inactive.png"
-    fourth.src = "https://s.gr-assets.com/images/layout/gr_red_star_inactive.png"
-    fifth.src = "https://s.gr-assets.com/images/layout/gr_red_star_inactive.png"
+  {    
+    this.setState({first: "https://s.gr-assets.com/images/layout/gr_red_star_active.png"})
+    this.setState({second: "https://s.gr-assets.com/images/layout/gr_red_star_active.png"})
+    this.setState({third: "https://s.gr-assets.com/images/layout/gr_red_star_inactive.png"})
+    this.setState({fourth: "https://s.gr-assets.com/images/layout/gr_red_star_inactive.png"})
+    this.setState({fifth: "https://s.gr-assets.com/images/layout/gr_red_star_inactive.png"})
+    this.setState({stars:2})
+    this.sendToDB()
+
   }
   three()
   {
-    var first = document.getElementById('i1');
-    var second = document.getElementById('i2');
-    var third = document.getElementById('i3');
-    var fourth = document.getElementById('i4');
-    var fifth = document.getElementById('i5');
-    first.src = "https://s.gr-assets.com/images/layout/gr_red_star_active.png"
-    second.src = "https://s.gr-assets.com/images/layout/gr_red_star_active.png"
-    third.src = "https://s.gr-assets.com/images/layout/gr_red_star_active.png"
-    fourth.src = "https://s.gr-assets.com/images/layout/gr_red_star_inactive.png"
-    fifth.src = "https://s.gr-assets.com/images/layout/gr_red_star_inactive.png"
+    this.setState({first: "https://s.gr-assets.com/images/layout/gr_red_star_active.png"})
+    this.setState({second: "https://s.gr-assets.com/images/layout/gr_red_star_active.png"})
+    this.setState({third: "https://s.gr-assets.com/images/layout/gr_red_star_active.png"})
+    this.setState({fourth: "https://s.gr-assets.com/images/layout/gr_red_star_inactive.png"})
+    this.setState({fifth: "https://s.gr-assets.com/images/layout/gr_red_star_inactive.png"})
+    this.setState({stars:3})
+    this.sendToDB()
   }
 
   four()
   {
-    var first = document.getElementById('i1');
-    var second = document.getElementById('i2');
-    var third = document.getElementById('i3');
-    var fourth = document.getElementById('i4');
-    var fifth = document.getElementById('i5');
-    first.src = "https://s.gr-assets.com/images/layout/gr_red_star_active.png"
-    second.src = "https://s.gr-assets.com/images/layout/gr_red_star_active.png"
-    third.src = "https://s.gr-assets.com/images/layout/gr_red_star_active.png"
-    fourth.src = "https://s.gr-assets.com/images/layout/gr_red_star_active.png"
-    fifth.src = "https://s.gr-assets.com/images/layout/gr_red_star_inactive.png"
+    this.setState({first: "https://s.gr-assets.com/images/layout/gr_red_star_active.png"})
+    this.setState({second: "https://s.gr-assets.com/images/layout/gr_red_star_active.png"})
+    this.setState({third: "https://s.gr-assets.com/images/layout/gr_red_star_active.png"})
+    this.setState({fourth: "https://s.gr-assets.com/images/layout/gr_red_star_active.png"})
+    this.setState({fifth: "https://s.gr-assets.com/images/layout/gr_red_star_inactive.png"})
+    this.setState({stars:4})
+    this.sendToDB()
   }
 
   five()
   {
-    var first = document.getElementById('i1');
-    var second = document.getElementById('i2');
-    var third = document.getElementById('i3');
-    var fourth = document.getElementById('i4');
-    var fifth = document.getElementById('i5');
-    first.src = "https://s.gr-assets.com/images/layout/gr_red_star_active.png"
-    second.src = "https://s.gr-assets.com/images/layout/gr_red_star_active.png"
-    third.src = "https://s.gr-assets.com/images/layout/gr_red_star_active.png"
-    fourth.src = "https://s.gr-assets.com/images/layout/gr_red_star_active.png"
-    fifth.src = "https://s.gr-assets.com/images/layout/gr_red_star_active.png"
+    this.setState({first: "https://s.gr-assets.com/images/layout/gr_red_star_active.png"})
+    this.setState({second: "https://s.gr-assets.com/images/layout/gr_red_star_active.png"})
+    this.setState({third: "https://s.gr-assets.com/images/layout/gr_red_star_active.png"})
+    this.setState({fourth: "https://s.gr-assets.com/images/layout/gr_red_star_active.png"})
+    this.setState({fifth: "https://s.gr-assets.com/images/layout/gr_red_star_active.png"})
+    this.setState({stars:5})
+    this.sendToDB()
   }
+
   render(){
     return(
       <div>
-        <img id="i1" alt="it was amazing" src="https://s.gr-assets.com/images/layout/gr_red_star_inactive.png" onClick={this.one.bind(this)}/>
-        <img id="i2" alt="" src="https://s.gr-assets.com/images/layout/gr_red_star_inactive.png" onClick={this.two.bind(this)}/>
-        <img id="i3" alt="" src="https://s.gr-assets.com/images/layout/gr_red_star_inactive.png" onClick={this.three.bind(this)}/>
-        <img id="i4" alt="" src="https://s.gr-assets.com/images/layout/gr_red_star_inactive.png" onClick={this.four.bind(this)}/>
-        <img id="i5" alt="" src="https://s.gr-assets.com/images/layout/gr_red_star_inactive.png" onClick={this.five.bind(this)}/> 
+        <img id="i1" alt="it was amazing" src={this.state.first} onClick={this.one.bind(this)}/>
+        <img id="i2" alt="" src={this.state.second} onClick={this.two.bind(this)}/>
+        <img id="i3" alt="" src={this.state.third} onClick={this.three.bind(this)}/>
+        <img id="i4" alt="" src={this.state.fourth} onClick={this.four.bind(this)}/>
+        <img id="i5" alt="" src={this.state.fifth} onClick={this.five.bind(this)}/> 
       </div>
     )
   }
