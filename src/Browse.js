f67986223715349e822e05e8685a667a43d3a060
Ampsss123 <117526504+Ampsss123@users.noreply.github.com>
2022-11-27 19:47:54 +0530

    Update Browse.js

--- src/Browse.js
+++ src/Browse.js
@@ -1,11 +1,67 @@
 import React from "react";
+import "./Stars.css";
+import myFavIm3 from './im1.jpg'
+import myFavIm4 from './im2.jpg'
+import myFavIm5 from './im3.jpg'
+import myFavIm6 from './im4.jpg'
+import myFavIm7 from './im5.jpg'
+import myFavIm8 from './im6.jpg'
+import myFavIm9 from './im7.jpg'
+import myFavIm10 from './im8.jpg'
+import myFavIm11 from './im9.jpg'
+import myFavIm12 from './im10.jpg'
+import myFavIm13 from './im11.jpg'
+import myFavIm14 from './im12.jpg'
+import myFavIm15 from './im13.jpg'
+import myFavIm16 from './im14.jpg'
+import myFavIm17 from './im15.jpg'
+import myFavIm18 from './im16.jpg'
+import myFavIm19 from './im17.jpg'
+import myFavIm20 from './im18.jpg'
+import myFavIm21 from './im19.jpg'
+import myFavIm22 from './im20.jpg'
+
+
+
+
 class Browse extends React.Component{
   render(){
     return(
       <div>
-        <h2>The search page</h2>
+        <h2> Wanna try new books?</h2>
+        <p>Fiction</p>
+        <img src={myFavIm3} />
+        <img src={myFavIm4} />
+        <img src={myFavIm5} />
+        <img src={myFavIm6} />
+        <p>Thriller</p>
+        <img src={myFavIm7} />
+        <img src={myFavIm8} />
+        <img src={myFavIm9} />
+        <img src={myFavIm10} />
+        <p>Romance</p>
+        <img src={myFavIm11} />
+        <img src={myFavIm12} />
+        <img src={myFavIm13} />
+        <img src={myFavIm14} />
+        <p>Comedy</p>
+        <img src={myFavIm15} />
+        <img src={myFavIm16} />
+        <img src={myFavIm17} />
+        <img src={myFavIm18} />
+        <p>Autobiography</p>
+        <img src={myFavIm19} />
+        <img src={myFavIm20} />
+        <img src={myFavIm21} />
+        <img src={myFavIm22} />
+        
+        
       </div>
     )
   }
 }
+
+
+
+
 export default Browse;
