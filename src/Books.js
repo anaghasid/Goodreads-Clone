6e05c8717e74d984c9a4f9987e057e576c39302b
Ampsss123 <117526504+Ampsss123@users.noreply.github.com>
2022-11-27 18:30:48 +0530

    Update Books.js

--- src/Books.js
+++ src/Books.js
@@ -38,6 +38,7 @@ export default My_Books;*/
 
 
 import React from "react";
+import Stars from'./Stars';
 import "./Books.css";
 import myFavImg5 from './perk.jpg';
 import myFavImg6 from './game.jpg';
@@ -45,21 +46,24 @@ class My_Books extends React.Component{
   render(){
     return(
       <div>
-        <h2>My books page</h2>
+        <h3>My books page</h3>
 
-  <div class="book2"> 
+  <div class="book2">
+      <div id="dynStars" title="it was amazing"><Stars name="Starts"/></div> 
       <img src={myFavImg5} />
       <p id="_title">The Perks of Being a Wallflower</p> 
-      <p id="_Author">Stephen Chbosky</p>
+      <p id="_Author">-Stephen Chbosky</p>
       <p id="_description">This is the story of what it's like to grow up in high school. More intimate than a diary, Charlie's letters are singular and unique, hilarious and devastating. We may not know where he lives. We may not know to whom he is writing. All we know is the world he shares. Caught between trying to live his life and trying to run from it puts him on a strange course through uncharted territory.
       </p>
    
   </div>
 
   <div class="book2"> 
+      <div id="dynStars" title="it was amazing"><Stars name="Starts"/></div>
       <img src={myFavImg6} />
+      
       <p id="_title">The Inheritance Games</p> 
-      <p id="_Author">Jennifer Lynn Barnes</p>
+      <p id="_Author">-Jennifer Lynn Barnes</p>
       <p id="_description">To receive her inheritance, Avery must move into sprawling, secret passage-filled Hawthorne House, where every room bears the old man's touch -- and his love of puzzles, riddles, and codes. Unfortunately for Avery, Hawthorne House is also occupied by the family that Tobias Hawthorne just dispossessed.
       </p>
 
